5bd4d105767d4b6bdd15e23ba1362798197b366d
SalahMSwefy <salahm.swefy@gmali.com>
2024-04-29 01:32:01 +0300

    # HBNB - The Console

--- models/place.py
+++ models/place.py
@@ -20,6 +20,10 @@ class Place(BaseModel, Base):
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
         reviews = relationship("Review", backref="place", cascade="all, delete")
+        place_amenity = Table('place_amenity', Base.metadata,
+                                Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+                                Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
+        amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
     else:
         city_id = ""
         user_id = ""
@@ -44,4 +48,22 @@ class Place(BaseModel, Base):
                     review_list.append(review)
             return review_list
 
+        @property
+        def amenities(self):
+            """ Getter attribute that returns the list of Amenity instances """
+            from models.amenity import Amenity
+            amenity_list = []
+            for amenity in list(models.storage.all(Amenity).values()):
+                if amenity.id in self.amenity_ids:
+                    amenity_list.append(amenity)
+            return amenity_list
+        
+        @amenities.setter
+        def amenities(self, obj):
+            """ Setter attribute that handles append method for adding an Amenity.id to the attribute amenity_ids """
+            from models.amenity import Amenity
+            if isinstance(obj, Amenity):
+                self.amenity_ids.append(obj.id)
+            
+        
         
