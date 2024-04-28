c206108007896bd39797a9ca24c5eecc26ac6cca
SalahMSwefy <salahm.swefy@gmali.com>
2024-04-28 23:41:41 +0300

    # HBNB - The Console

--- models/state.py
+++ models/state.py
@@ -16,13 +16,14 @@ class State(BaseModel, Base):
     else:
         name = ""
     
-    @property
-    def cities(self):
-        """getter attribute cities that returns the list of City instances"""
-        from models import storage
-        city_list = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """getter attribute cities that returns the list of City instances"""
+            from models import storage
+            city_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
     
