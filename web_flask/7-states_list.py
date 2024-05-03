0c40c71c2530e131e1ba8c140b1b17d34c5f7317
SalahMSwefy <salahm.swefy@gmali.com>
2024-05-03 18:18:31 +0300

    # HBNB - The Console

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,51 +6,6 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    """Return a string"""
-    return "Hello HBNB!"
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """Return a string when /hbnb is called"""
-    return "HBNB"
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """Return a string when /c/<text> is called"""
-    message = text.replace("_", " ")
-    return "C {}".format(message)
-
-
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python(text='is cool'):
-    """Return a string when /python/<text> is called"""
-    message = text.replace("_", " ")
-    return "Python {}".format(message)
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """Return a string when /number/<n> is called"""
-    return "{} is a number".format(n)
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """Return a string when /number_template/<n> is called"""
-    return render_template('5-number.html', n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    """Return a string when /number_odd_or_even/<n> is called"""
-    return render_template('6-number_odd_or_even.html', n=n)
-
-
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Return a string when /states_list is called"""
